a2beee4fc094f20caa6576707eee26c39f52af08
Sean Cleveland <sean.b.cleveland@gmail.com>
2011-05-26 17:17:05 -0600

    Updated MetaTags model

--- app/models/voeis/meta_tag.rb
+++ app/models/voeis/meta_tag.rb
@@ -5,13 +5,15 @@ class Voeis::MetaTag
   
   property :id,       Serial
   #property :name,     String,  :required => true
-  property :value,    Text,   :required => true
-  property :name,     String, :required => true, :length => 512
-  property :category, String, :required => true, :length => 512
+  property :value,    Text,   :required => true, :required => false
+  property :name,     String, :required => true, :length => 512, :index => true
+  property :category, String, :required => true, :length => 512, :index => true
 
+  timestamps :at
   yogo_versioned
 
   has n, :sensor_values, :model => 'Voeis::SensorValue', :through => Resource
   has n, :data_values, :model => 'Voeis::DataValue', :through => Resource
   has n, :variables, :model => 'Voeis::Variable', :through => Resource
+
 end
