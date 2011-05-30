da606ff16c048ea8f78fc2597e53eb48857de186
Sean Cleveland <sean.b.cleveland@gmail.com>
2011-05-30 16:17:52 -0600

    Created a Project Field Method Controller

---
+++ app/controllers/voeis/field_methods_controller.rb
@@ -0,0 +1,42 @@
+class Voeis::FieldMethodsController < Voeis::BaseController
+  # Properly override defaults to ensure proper controller behavior
+  # @see Voeis::BaseController
+  defaults  :route_collection_name => 'field_methods',
+            :route_instance_name => 'field_method',
+            :collection_name => 'field_methods',
+            :instance_name => 'field_method',
+            :resource_class => Voeis::FieldMethod
+
+  def new
+    @project = parent
+    @field_methods = Voeis::FieldMethod.all
+  end
+
+  def edit
+    @field_method =  parent.managed_repository{Voeis::FieldMethod.get(params[:id])}
+    @project = parent
+  end
+
+  def create
+    parent.managed_repository do
+      # if params[:field_method].nil?
+      #   @field_method = Voeis::FieldMethod.new(:field_name=> params[:field_name], :field_organization => params[:field_organization], :field_method_name => params[:field_method_name], :field_method_description => params[:field_method_description])
+      # else
+        @field_method = Voeis::FieldMethod.new(params[:field_method])
+      # end
+      respond_to do |format|
+        if @field_method.save
+          flash[:notice] = 'Field Method was successfully created.'
+           format.json do
+              render :json => @field_method.as_json, :callback => params[:jsoncallback]
+            end
+          format.html { (redirect_to(new_project_field_method_path())) }
+          format.js
+        else
+          format.html { render :action => "new" }
+        end
+      end
+    end
+  end
+
+end
