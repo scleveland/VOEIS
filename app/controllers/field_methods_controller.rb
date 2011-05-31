6a8d99c33f51d7ce35bc7ae1a76e38bbfd5501a6
Sean Cleveland <sean.b.cleveland@gmail.com>
2011-05-30 23:40:15 -0600

    Changed the Create function to save correctly and return the correct JSON response

--- app/controllers/field_methods_controller.rb
+++ app/controllers/field_methods_controller.rb
@@ -13,15 +13,15 @@ class FieldMethodsController < ApplicationController
   
   # POST /field_methods
   def create
-    if params[:field_method].nil?
-      @field_method = Voeis::FieldMethod.new(:method_description=> params[:field_method_description], :method_link => params[:method_link])
-    else
-      @field_method = Voeis::FieldMethod.new(params[:field_method])
-    end
+    @field_method = Voeis::FieldMethod.new(params[:field_method])
+    
     respond_to do |format|
       if @field_method.save
         flash[:notice] = 'Field Method was successfully created.'
         format.html { (redirect_to(new_field_method_c_v_path())) }
+        format.json do
+          render :json => @field_method.as_json, :callback => params[:jsoncallback]
+        end
       else
         format.html { render :action => "new" }
       end
