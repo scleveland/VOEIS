2fe72bd3deba777162ce3aeb782c4c8e50831fec
Sean Cleveland <sean.b.cleveland@gmail.com>
2011-05-31 23:34:29 -0600

    Added Spatial Offset supprot

---
+++ app/models/voeis/spatial_offset.rb
@@ -0,0 +1,16 @@
+class Voeis::SpatialOffset
+  include DataMapper::Resource
+  include Yogo::Versioned::DataMapper::Resource
+
+  property :id,                 Serial
+  property :type,               String, :required =>true, :length=> 512, :default=>"empty"
+  property :value,              String,   :required => true
+  property :units_id,           Integer,   :required => false
+  
+  timestamps :at
+  yogo_versioned
+ 
+  has n, :variables, :model => "Voeis::Variable", :through => Resource
+  #has 1, :unit,      :model => "Voeis::Unit",     :through => Resource
+
+end
