19bbe90e1bab59e8f77f85f2d9d6144e6231781d
Sean Cleveland <sean.b.cleveland@gmail.com>
2011-07-28 16:54:47 -0600

    updated the sample and logger parsing interfaces

--- app/controllers/voeis/data_values_controller.rb
+++ app/controllers/voeis/data_values_controller.rb
@@ -1320,11 +1320,11 @@ class Voeis::DataValuesController < Voeis::BaseController
            Voeis::Site.get(site.id).update_site_data_catalog_variables(@variables)
          end #end repo
          #parent.publish_his
-         flash[:notice] = "File parsed and stored successfully."
+         flash[:notice] = "File parsed and stored successfully for #{site.name}."
          redirect_to project_path(params[:project_id]) and return
          rescue Exception => e  
            email_exception(e,request.env)
-           flash[:error] = "Problem Parsing Logger File: "+ e.message
+           flash[:error] = "Problem Parsing Sample File: "+ e.message
            redirect_to(:controller =>"voeis/data_values", :action => "pre_process_samples_file_upload", :params => {:id => params[:project_id]})
          end
    end# end def
