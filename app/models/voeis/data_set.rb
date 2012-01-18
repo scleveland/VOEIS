63fcb7ed02e13f5114f0d9a62a7489b9f83a32c1
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-01-18 15:14:47 -0700

    updated the API function to add data to a data set

--- app/models/voeis/data_set.rb
+++ app/models/voeis/data_set.rb
@@ -9,6 +9,7 @@ class Voeis::DataSet
   property :name,        String,  :required => true, :unique => true, :length => 512
   property :type,        String, :required => true, :default => "default"
   property :description, Text,    :required => false
+  property :data_value_count, Integer, :required => false
   
   yogo_versioned
 
@@ -25,20 +26,31 @@ class Voeis::DataSet
     results.map{|v| Voeis::Variable.get(v)}
   end
   
+  def update_count
+    sql = "UPDATE voeis_data_sets SET data_value_count=#{self.data_values.count} WHERE id=#{self.id}"
+    repository.adapter.execute(sql)
+  end
+  
   def count
-    self.data_values.count
+    self.data_values_count
   end
   
   def get_dv_by_var_id(var_id)
     self.data_values.all(:variable_id => var_id)
   end
   
+  # This will ignore data_values
   def add_data_values(data_value_ids)
+    int_data_value_ids = data_value_ids.map{|k| k.to_i}
+    current_ids = self.data_values.map{|k| k.id}
     data_sql = Array.new
-    data_sql = data_value_ids.map{|data_value_id| "(#{self.id},#{data_value_id})"}
-    sql = "INSERT INTO \"voeis_data_set_data_values\" (\"data_set_id\",\"data_value_id\") VALUES "
-    puts sql << data_sql.join(',')
-    repository.adapter.execute(sql)
+    unless (int_data_value_ids-current_ids).empty?
+      data_sql = (int_data_value_ids-current_ids).map{|data_value_id| "(#{self.id},#{data_value_id})"}
+      sql=  "INSERT INTO \"voeis_data_set_data_values\" (\"data_set_id\",\"data_value_id\") VALUES "
+      puts sql << data_sql.join(',')
+      repository.adapter.execute(sql)
+      self.update_count
+    end
     self.data_values
   end
   
