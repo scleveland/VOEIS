67e3caaa6b5df6f3b0c1eb1280c10c061d23e8ec
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-01-26 12:46:54 -0700

    updating search

--- app/controllers/voeis/search_controller.rb
+++ app/controllers/voeis/search_controller.rb
@@ -2,9 +2,16 @@ class Voeis::SearchController < Voeis::BaseController
   # Properly override defaults to ensure proper controller behavior
   # @see Voeis::BaseController
   
-  def index
+  def new
     @sites = parent.managed_repository{Voeis::Site.all}
     @variables = parent.managed_repository{Voeis::Variable.all}
+    @units = Voeis::Unit.all
+    @unit_names = Hash.new
+    @units.map{|u| @unit_names = @unit_names.merge({u.id => u.units_name})}
+  end
+  
+  def index
+  
   end
 
 end
