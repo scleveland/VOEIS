5e2e640ef14815cb392b3e19541956477912940c
Pol Llovet <pol.llovet@gmail.com>
2012-01-27 15:05:02 -0700

    Initial search and search result test.

--- app/controllers/voeis/search_controller.rb
+++ app/controllers/voeis/search_controller.rb
@@ -11,7 +11,31 @@ class Voeis::SearchController < Voeis::BaseController
   end
   
   def index
-  
+    site_ids    = params[:site_ids].split(',')
+    variable_ids = params[:var_ids].split(',')
+    start_date   = params[:start_date]
+    end_date     = params[:end_date]
+    
+    data = parent.managed_repository {
+      Voeis::DataValue.all(:variable_id => variable_ids, 
+                            :site_id => site_ids,
+                            :local_date_time.gte => start_date,
+                            :local_date_time.lte => end_date)
+    }
+
+    results = []
+    # result[timestamp] = {var_id=>val,var_id=>val,var_id=>val}
+    data.each do |d|
+      results[d.local_date_time] ||= {}
+      results[d.local_date_time][d.variable_id] = d.data_value
+    end
+    
+    @data = results.map{|k,v| {:timestamp => k}.merge(v) }
+    @data = @data.sort{|a,b| a[:timestamp] <=> b[:timestamp] }
+    
+    @variables = parent.managed_repository {
+      Voeis::Variable.all(:id => variable_ids)
+    }
   end
 
 end
