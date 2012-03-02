9ca6fd5b73f8a40d8c0363903064174572c831da
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-03-02 16:10:04 -0700

    added a struct to_hash method and a sql_to_json method for Arrays

--- config/environment.rb
+++ config/environment.rb
@@ -15,7 +15,19 @@ require 'rack/rql'
 Yogo::Application.configure do
   config.middleware.insert_before(ActionDispatch::Head, Rack::RqlQuery)
 end
-
+#extend the Struct class
+class Struct
+   def to_hash
+     Hash[*members.zip(values).flatten]
+   end
+ end
+#extend Array so that raw sql results can be converted to json easily
+#assumes this is an array of Structs or Objects that have the to_hash method
+class Array
+  def sql_to_json
+    self.map{|k| k.to_hash}.as_json
+  end
+end
 # Initialize the rails application
 Yogo::Application.initialize!
 
