913dee0b667143d691bbe467b1c9d535d019b5a7
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-03-27 12:42:29 -0600

    swapped update site variable to update site when catching exception.

--- app/controllers/voeis/data_values_controller.rb
+++ app/controllers/voeis/data_values_controller.rb
@@ -1561,7 +1561,7 @@ class Voeis::DataValuesController < Voeis::BaseController
          redirect_to project_path(params[:project_id]) and return
          rescue Exception => e  
            email_exception(e,request.env)
-           Voeis::Site.get(site.id).update_site_data_catalog_variables(@variables)
+           Voeis::Site.get(site.id).Voeis::Site.get(site.id).update_site_data_catalog
            flash[:error] = "Problem Parsing Sample File: "+ e.message
            redirect_to(:controller =>"voeis/data_values", :action => "pre_process_samples_file_upload", :params => {:id => params[:project_id]})
          end
