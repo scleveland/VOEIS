58f0198cf112c6bb0c940b43583e62f0636e4862
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-05-18 14:53:21 -0600

    changed menu links to open in tab if they are supposed to. changed help docs url to github-pages

--- public/javascripts/yogo/ui/MenuLink.js
+++ public/javascripts/yogo/ui/MenuLink.js
@@ -1,11 +1,28 @@
 dojo.provide("yogo.ui.MenuLink");
+dojo.provide("yogo.ui.MenuLinkBlankTarget");
 dojo.require("dijit.MenuItem");
 
 dojo.declare("yogo.ui.MenuLink", dijit.MenuItem, {
     href: "",
     onClick: function(evt) {
-        if(this.href) {
-            window.location = this.href;
+        if(this.target == "_blank"){
+          window.open(this.href,'_blank');
         }
+        else{
+          if(this.href) {
+              window.location = this.href;
+          }
+        }
+    }
+});
+
+
+dojo.declare("yogo.ui.MenuLinkBlankTarget", dijit.MenuItem, {
+    href: "",
+    onClick: function(evt) {
+
+          if(this.href) {
+              window.open(this.href,'_blank');
+          }
     }
 });
