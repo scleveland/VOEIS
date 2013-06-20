8cda8432256be8b6cf5fbb429e88a7d08174165b
Sean Cleveland <sean.b.cleveland@gmail.com>
2013-06-20 15:47:15 -0600

    change docket dir

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -33,7 +33,7 @@ working_directory APP_PATH
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/path/to/.unicorn.sock", :backlog => 64
+listen "/var/rails/current/.unicorn.sock", :backlog => 64
 listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
